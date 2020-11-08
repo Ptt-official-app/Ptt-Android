e504c61ae2151d7452f0064615a243ec2b92365a
Takuma <vmgsahm1@gmail.com>
2020-11-08 19:34:33 +0800

    Convert to fragment navigation in HomeActivity.

--- app/src/test/java/tw/y_studio/ptt/Utils/StringUtilsTest.java
+++ app/src/test/java/tw/y_studio/ptt/Utils/StringUtilsTest.java
@@ -1,13 +1,13 @@
 package tw.y_studio.ptt.Utils;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StringUtilsTest {
 
