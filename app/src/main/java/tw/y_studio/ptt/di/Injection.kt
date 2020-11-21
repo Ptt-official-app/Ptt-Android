c0f4b4cb1a843b00933016252aad8d1eb5372a33
Michael Lien <ininmm830120@gmail.com>
2020-11-21 20:06:20 +0800

    Refactor PostListAPIHelper.java

--- app/src/main/java/tw/y_studio/ptt/di/Injection.kt
+++ app/src/main/java/tw/y_studio/ptt/di/Injection.kt
@@ -1,9 +1,12 @@
 package tw.y_studio.ptt.di
 
 import tw.y_studio.ptt.api.PopularBoardListAPI
+import tw.y_studio.ptt.api.PostListAPI
 import tw.y_studio.ptt.api.SearchBoardAPI
 import tw.y_studio.ptt.source.remote.popular.IPopularRemoteDataSource
 import tw.y_studio.ptt.source.remote.popular.PopularRemoteDataSourceImpl
+import tw.y_studio.ptt.source.remote.post.IPostListRemoteDataSource
+import tw.y_studio.ptt.source.remote.post.PostListRemoteDataSourceImpl
 import tw.y_studio.ptt.source.remote.search.ISearchBoardRemoteDataSource
 import tw.y_studio.ptt.source.remote.search.SearchBoardRemoteDataSourceImpl
 
@@ -13,6 +16,7 @@ object Injection {
             PopularBoardListAPI()
         }
         val searchBoardAPI by lazy { SearchBoardAPI() }
+        val postListAPI by lazy { PostListAPI() }
     }
 
     object RemoteDataSource {
@@ -21,6 +25,9 @@ object Injection {
         }
         val searchBoardRemoteDataSource: ISearchBoardRemoteDataSource by lazy {
             SearchBoardRemoteDataSourceImpl(API.searchBoardAPI)
+        }
+        val postListRemoteDataSource: IPostListRemoteDataSource by lazy {
+            PostListRemoteDataSourceImpl(API.postListAPI)
         }
     }
 }
