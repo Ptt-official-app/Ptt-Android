306a3b6ab21ab1fdd50b56cc5a8634be0fdfb492
Michael Lien <ininmm830120@gmail.com>
2021-05-16 16:37:06 +0800

    Add article set rank api

--- app/src/main/java/tw/y_studio/ptt/source/remote/article/ArticleRemoteDataSourceImpl.kt
+++ app/src/main/java/tw/y_studio/ptt/source/remote/article/ArticleRemoteDataSourceImpl.kt
@@ -1,7 +1,10 @@
 package tw.y_studio.ptt.source.remote.article
 
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
 import tw.y_studio.ptt.api.PostAPI
 import tw.y_studio.ptt.api.PostRankMark
 import tw.y_studio.ptt.api.article.ArticleApiService
@@ -9,6 +12,7 @@ import tw.y_studio.ptt.api.model.Post
 import tw.y_studio.ptt.api.model.PostRank
 import tw.y_studio.ptt.api.model.article.ArticleCommentsList
 import tw.y_studio.ptt.api.model.article.ArticleDetail
+import tw.y_studio.ptt.api.model.article.ArticleRank
 
 class ArticleRemoteDataSourceImpl(
     private val postAPI: PostAPI,
@@ -29,6 +33,19 @@ class ArticleRemoteDataSourceImpl(
         desc: Boolean
     ): ArticleCommentsList = withContext(dispatcher) {
         return@withContext articleApiService.getArticleComments(boardId, articleId, desc)
+    }
+
+    override suspend fun postArticleRank(
+        rank: Int,
+        boardId: String,
+        articleId: String
+    ) = withContext(dispatcher) {
+        val param: String = Gson().toJson(
+            ArticleRank(rank)
+        )
+        val body = param.toRequestBody("text/plain; charset=utf-8".toMediaType())
+
+        return@withContext articleApiService.postArticleRank(boardId, articleId, body)
     }
 
     override fun getPost(board: String, fileName: String): Post {
